d95f9f1acb4574a7d33035113544ca205a6f68bf
Ahnjaewon <vutus5@naver.com>
2023-11-14 14:50:48 +0900

    login update

---
+++ src/components/global/routes/GetRoleRoute.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+import {useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
+
+import cookie from "react-cookies";
+import {Outlet} from "react-router";
+import {tokenAtom} from "../atom/LoginAtom";
+
+const GetRoleRoute = () => {
+
+    const [token,setToken] = useRecoilState(tokenAtom);
+    useEffect(() => {
+        const myCookieValue = cookie.load('AccessToken');
+        console.log(myCookieValue);
+        setToken(myCookieValue);
+    }, []);
+    return <div>
+        <Outlet/>
+    </div>
+};
+
+export default GetRoleRoute;
