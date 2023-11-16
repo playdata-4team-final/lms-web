7a69b96eb01e198e7042230088ee1953fb125f45
Ahnjaewon <67565707+Ahnjaewon@users.noreply.github.com>
2023-11-16 10:24:53 +0900

    Update GetRoleRoute.jsx

--- src/components/global/routes/GetRoleRoute.jsx
+++ src/components/global/routes/GetRoleRoute.jsx
@@ -1,18 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import {useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
 
-import cookie from "react-cookies";
+
 import {Outlet} from "react-router";
-import {tokenAtom} from "../atom/LoginAtom";
+import {idAtom, roleAtom, tokenAtom} from "../atom/LoginAtom";
+import {exceptionApi} from "../api/Api";
 
 const GetRoleRoute = () => {
 
-    const [token,setToken] = useRecoilState(tokenAtom);
+    const [id,setId] = useRecoilState(idAtom);
+    const [role, setRole] = useRecoilState(roleAtom);
+
     useEffect(() => {
-        const myCookieValue = cookie.load('AccessToken');
-        console.log(myCookieValue);
-        setToken(myCookieValue);
+        get();
     }, []);
+
+    const get = async () => {
+        const response = await exceptionApi('/api/v1/auth/info', 'POST');
+        setId(response.data.id)
+        setRole(response.data.role)
+        console.log(response.data.id)
+        console.log(response.data.role)
+        return response;
+    }
     return <div>
         <Outlet/>
     </div>
